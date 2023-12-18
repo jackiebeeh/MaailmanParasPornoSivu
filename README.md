36d599a8a66e8badcb63593a36f2b094221da10b
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2023-12-18 12:23:14 +0200

    added the live website address to the README file

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ A fun project I'm building for a friend
 
 Imitating a porn site BUT better than all the others.
 
+View live website at:
+https://jackiebeeh.github.io/MaailmanParasPornoSivu/
+
 Designed and built by Sanelma Brown (GitHub @Jackiebeeh)
 
 **********************************************************************
--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// $(document).ready(function() {
-//   setTimeout(function() {
-//     $("body").addClass("loaded");
-//   },3000);
-// });
+// Loading page:
 const loader = document.getElementById("loader");
 
 window.addEventListener("load", () => {
   document.body.classList.add("loaded");
 });
 
-const blurImg = document.getElementsByClassName("blur-img");
-
-let img = document.getElementById("heroImage");
-let dropdownContent = document.querySelector(".dropdown-content");
-let options = dropdownContent.getElementsByTagName('a')
+// Menu Images:
 const images = [];
-
 images[0] = "./images/imgZERO.jpeg";
 images[1] = "./images/imgONE.jpeg";
 images[2] = "./images/imgTWO.jpeg";
@@ -26,15 +17,23 @@ images[6] = "./images/imgSIX.jpeg";
 images[7] = "./images/imgSEVEN.jpeg";
 images[8] = "./images/imgEIGHT.jpeg";
 
-
-
+let img = document.getElementById("heroImage");
 img.innerHTML = `<img src="${images[0]}" />`;
+
+// let imgDiv = document.querySelector(".heroImage");
+// let img = imgDiv.createElement("img", {src =`${images[0]`});
+
+const blurImg = document.getElementsByClassName("blur-img");
+
+// Dropdown menu:
+let dropdownContent = document.querySelector(".dropdown-content");
+let options = dropdownContent.getElementsByTagName('a')
+
+
 
 // options.onmouseenter = () => {
   for(let i = 0; i < options.length||i < images.length; i++) {
     options[i].addEventListener("mouseover", function(){
-      // images[i].classList.remove("transition2");
-      // images[i].setAttribute("class", "transition1");
       img.innerHTML = `<img src="${images[i+1]}" />`;
     })
   }
@@ -42,11 +41,15 @@ img.innerHTML = `<img src="${images[0]}" />`;
 // options.onmouseleave = () => {
   for(let i = 0; i < options.length||i < images.length; i++) {
     dropdownContent.addEventListener("mouseout", function(){
-      // images.classList.remove("transition1");
-      // images.setAttribute("class", "transition2");
-
       img.innerHTML = `<img src="${images[0]}" />`;
     })
   }
 // }
+
+// ****************************************************
+// $(document).ready(function() {
+//   setTimeout(function() {
+//     $("body").addClass("loaded");
+//   },3000);
+// });
 
